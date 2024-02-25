aa95831a5b44fa4c63b2ed600fa1c08532255b78
Flantoro <steculalviv@gmail.com>
2024-02-25 11:17:03 +0200

    Tests added

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -57,7 +57,7 @@ describe('Webdriverio main page', () => {
         console.log("place holser is " + attr);
     })
 
-    it('Home Task', async () => {
+    xit('Home Task', async () => {
         await browser.url('https://webdriver.io/');
 
         let drop = await $("[aria-label='Toggle navigation bar']");
@@ -77,5 +77,82 @@ describe('Webdriverio main page', () => {
         await browser.pause(1000);
         await input.setValue("");
     })  
+    xit('IsClickable method', async () => {
+        await browser.url('https://webdriver.io/');
+
+        const button = await $(".button[href='/docs/gettingstarted']");
+        let clickable = await button.isClickable();
+        console.log("Button is displayed " + clickable);
+    });
+
+    xit('IsVisible method', async () => {
+        await browser.url('https://webdriver.io/');
+
+        const visible = await $(".button[href='/docs/gettingstarted']");
+        let displayed = await visible.isDisplayed();
+        console.log("Button is clickable " + displayed);
+    });  
+
+    xit('IsEnabled method', async () => {
+        await browser.url('https://webdriver.io/');
+
+        const button = await $(".button[href='/docs/gettingstarted']");
+        let enabled = await button.isEnabled();
+        console.log("Button is enabled " + enabled);
+    });
+
+    xit('Scroll method', async () => {
+        await browser.url('https://webdriver.io/');
+
+        const FooterButton = await $(".footer__link-item[href='/docs/gettingstarted']");
+        await browser.pause(1000);
+        await FooterButton.scrollIntoView();
+        await browser.pause(1000);
+    });
+
+    xit('saveScreenshot method', async () => {
+        await browser.url('https://webdriver.io/');
+
+        const FooterButton = await $(".footer__link-item[href='/docs/gettingstarted']");
+        await browser.pause(1000);
+        await FooterButton.scrollIntoView();
+        await browser.pause(1000);
+        await FooterButton.saveScreenshot("scr.png");
+    });
+
+    xit('newWindow and switchWindow methods', async () => {
+        await browser.url('https://webdriver.io/');
+
+        await browser.newWindow('https://github.com/Flantoro');
+        await browser.switchWindow('https://webdriver.io/');
+    });
+
+    it('Home Task 2', async () => {
+        await browser.url('https://webdriver.io/');
+
+        let drop = await $("[aria-label='Toggle navigation bar']");
+        await drop.click();
+        let api = await $("/html/body/div/nav/div[3]/div[2]/div[1]/ul/li[2]/a");
+        await api.click();
+        let blogButton = await $("a[href='/blog'][class='navbar__item navbar__link']");
+        await blogButton.scrollIntoView();
+
+        let protocolButton = await $("div [class='pagination-nav__label']");
+        let visible = await protocolButton.isDisplayed();
+        console.log("Button is visible" + visible);
+        await protocolButton.click();
+
+        let htm = await $("div h1");
+        console.log(await htm.getHTML());
+
+        let element = await $("div h2[id='webdriver-protocol']");
+        await element.waitUntil(async function () {
+            return (await this.getText()) === 'WebDriver Protocol'
+        }, {
+            timeout: 5000,
+            timeoutMsg: 'expected text to be different after 5s'
+        })
+    })  
+
 })
 
