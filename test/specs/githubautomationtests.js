b44e0d10eca649e3c566ca58d851811abaa8d8ec
Flantoro <steculalviv@gmail.com>
2024-02-26 13:11:30 +0200

    Tests added

--- test/specs/githubautomationtests.js
+++ test/specs/githubautomationtests.js
@@ -12,7 +12,7 @@ describe('GitHub Test Cases', () => {
         await expect(aboutText).toBeDisplayedInViewport();
     })
 
-    it('Subscribe to a developers newsletters', async () => {
+    xit('Subscribe to a developers newsletters', async () => {
         await browser.url('https://github.com/');
         const subscribeButton = await $("[href='https://resources.github.com/newsletter/']");
         await subscribeButton.scrollIntoView();
@@ -31,5 +31,50 @@ describe('GitHub Test Cases', () => {
         await subButton.click();
 
         await expect(await $("div h1")).toHaveText("Thanks for subscribing!");
+    })
+
+    xit('Try to subscribe to a developers newsletters withount entering email', async () => {
+        await browser.url('https://github.com/');
+        const subscribeButton = await $("[href='https://resources.github.com/newsletter/']");
+        await subscribeButton.scrollIntoView();
+        await subscribeButton.click();
+        const emailInputField = await $("[placeholder='you@company.com']");
+        await expect(emailInputField).toBeDisplayed();
+        const uaID = await $("[value='UA']");
+        await uaID.click();
+        await expect(uaID).toBeSelected();
+
+        const subButton = await $("button span span");
+        await subButton.click();
+
+        
+    })
+
+    xit('Try to subscribe for free for the GitHub without login', async () => {
+        await browser.url('https://github.com/');
+        const dropDown = await $("div[class='flex-1 flex-order-2 text-right'] span span");
+        await dropDown.click();
+        const pricingbeButton = await $("nav ul li a[href='/pricing']");
+        await pricingbeButton.click();
+        
+        await expect(await $("h1[class='h2-mktg']")).toHaveText("Get the complete developer platform.");
+
+        const freePlanButton = await $("[test_selector='plan-input-free']");
+        await freePlanButton.click();
+
+
+        await expect(browser).toHaveUrl("https://github.com/signup");
+    })
+
+    it('Open the GitHub Ducumentation page from the header', async () => {
+        await browser.url('https://github.com/');
+        const dropDown = await $("div[class='flex-1 flex-order-2 text-right'] span span");
+        await dropDown.click();
+        const productButton = await $("//*/button[contains(text(), 'Product')]");
+        await productButton.click();
+        const documentationButton = await $("header  div ul li a[href='https://docs.github.com']");
+        await documentationButton.click();
+        await browser.switchWindow("https://docs.github.com/");
+        await expect(browser).toHaveUrl("https://docs.github.com/en");
     })
 })
